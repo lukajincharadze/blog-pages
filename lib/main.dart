b30576afbd88304ac42d0a9983526348469b6c23
Luka Jincharadze <lukajinchara@gmail.com>
2023-02-22 22:50:57 +0400

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -21,35 +21,84 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "assets/img/flutter.png",
-                  width: 50,
-                  height: 50,
-                ),
-                Column(
-                  children: [
-                    Container(
-                      child: Text(
-                        "Flutter Is Awesome",
-                        style: TextStyle(fontSize: 28, color: Colors.yellow),
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+              color: Colors.green,
+              width: 500,
+              height: 130,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/img/flutter.png",
+                    width: 50,
+                    height: 50,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        child: Text(
+                          "Flutter Is Awesome",
+                          style: TextStyle(fontSize: 28, color: Colors.yellow),
+                        ),
+                        padding: EdgeInsets.only(top: 36),
                       ),
-                    ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(left: 20),
-                      child: Text(
-                        "Flutter is Cross-Platform UI Framework",
-                        style: TextStyle(fontSize: 15, color: Colors.yellow),
+                      SizedBox(
+                        height: 5,
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Container(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(
+                          "Flutter is Cross-Platform UI Framework",
+                          style: TextStyle(fontSize: 15, color: Colors.yellow),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+              color: Colors.yellow,
+              width: 500,
+              height: 130,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/img/dart.png",
+                    width: 50,
+                    height: 50,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        child: Text(
+                          "Dart Is Cool Too",
+                          style: TextStyle(fontSize: 28, color: Colors.green),
+                        ),
+                        padding: EdgeInsets.only(top: 36, right: 39),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(
+                          "Dart is modern programming language",
+                          style: TextStyle(fontSize: 15, color: Colors.green),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
